6bcd54d8153e9273b734dd98e021572ef1769c87
alexastagr <contact@alexasta.gr>
2025-08-22 11:25:29 -0700

    perform backup

--- backupi.sh
+++ backupi.sh
@@ -14,3 +14,28 @@ FTP_DIR="/remote/backup/path"
 
 # create DIR if not exist
 mkdir -p "$BACKUP_DIR/$DATE"
+
+
+while IFS= read -r LINE; do
+    
+    [ -z "$LINE" ] && continue
+
+    eval $(echo "$LINE" | tr '&' ' ')
+
+    if [ -z "$database" ] || [ -z "$user" ] || [ -z "$password" ]; then
+        echo "⚠️ Invalid line: $LINE"
+        continue
+    fi
+
+    echo "Backing up: $database@$databasehost"
+
+    mysqldump -h "$databasehost" -u "$user" -p"$password" "$database" \
+    | gzip > "$BACKUP_DIR/$DATE/${database}_$DATE.sql.gz"
+
+    if [ $? -eq 0 ]; then
+        echo "Backup completed for $database"
+    else
+        echo "Backup failed for $database"
+    fi
+
+done < "$DB_LIST"
